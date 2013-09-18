a897bf67a3231ed775e238777354f43db76815b3
Alberto Cole <acpii2005@gmail.com>
2013-09-18 13:03:56 -0600

    Mejoras para el chat

--- config/sockets.js
+++ config/sockets.js
@@ -1,22 +1,24 @@
 var _ = require('lodash');
 
 module.exports = function (io) {
-  var socket_list = {};
+  var socket_list = {},
+    CETAV_KEY = 'CETAV';
   io.sockets.on('connection', function(socket) {
+    socket.join(CETAV_KEY);
     socket.on('chat:registrar',function(nombreUsuario,callback){
-      if(_.contains(socket_list,nombreUsuario)){
-        callback(false);
-      }else{
-        callback(true);
-        socket.nickname = nombreUsuario;
-        socket_list[socket.nickname] = socket;
-      }
+      socket.nickname = nombreUsuario;
+      callback(true);
+      socket_list[socket.nickname] = socket;
+      socket.emit('chat:lista',Object.keys(socket_list));
     });
     socket.on('chat:susurro',function(data){
       socket_list[data.usuarioDestinatario].emit('chat:whisper', {cuerpo: data.mensaje, usuario: socket.nickname});
     });
     socket.on('chat:todos',function(data){
       io.sockets.emit('chat:mensaje', {cuerpo: data.mensaje, usuario: data.usuario});
+    });
+    socket.on('chat:lista',function(callback){
+      callback(Object.keys(socket_list));
     });
   });
 }
